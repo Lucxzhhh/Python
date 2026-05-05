e2403dbf0dd848ff1f38672a2573a5a159f344b8
Lucas <everaldotinettilucas@gmail.com>
2026-05-05 15:22:35 -0300

    Update variable definition and add math operations
    refactor: melhora organização e clareza no código de variáveis

--- basico/variaveis.py
+++ basico/variaveis.py
@@ -1,18 +1,33 @@
 # Cada tipo de dado guarda uma informação específica
 
 nome = "Lucas"    # String: guarda palavras ou frases
-
 idade = 18        # Int: guarda números inteiros (sem decimal)
-
 altura = 1.80     # Float: guarda números decimais (usa ponto)
-
 certo = True      # Bool: verdadeiro (sim)
 errado = False    # Bool: falso (não)
 
 
 # O que é uma variável?
 
-# Variável é um idade que guarda um valor na memória
+# Variável é um nome que guarda um valor na memória
 # Exemplo:
-
 idade = 18
+
+
+# Operações matemáticas com variáveis
+
+numero1 = 10
+numero2 = 10
+resultado = numero1 + numero2  # Adição (+)
+
+numero1 = 10
+numero2 = 5
+resultado = numero1 - numero2  # Subtração (-)
+
+numero1 = 2
+numero2 = 10
+resultado = numero1 * numero2  # Multiplicação (*)
+
+numero1 = 20
+numero2 = 2
+resultado = numero1 / numero2  # Divisão (/)
