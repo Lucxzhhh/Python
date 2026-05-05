afb3f22e6a1a02f8736613951167b5b0d3db53f5
Lucas <everaldotinettilucas@gmail.com>
2026-05-05 14:57:12 -0300

    Update comandos.py
    docs: adiciona explicação simples sobre variáveis

--- basico/comandos.py
+++ basico/comandos.py
@@ -1,1 +1,10 @@
+# Cada tipo de variável guarda uma informação específica :
 
+Nome = "Lucas"    # Tipo da variável String (Guarda palavras ou frases).
+
+Idade = 18        # Tipo da variável Int (Guarda números inteiros, números sem virgula).
+
+Altura = 1.80     # Tipo da variável Float (Guarda números decimais, números com vírgula ).
+
+Certo = True      # Tipo da variável Bool (Serve para respostas de sim ou não).
+Errado = False    # Tipo da variável Bool (Serve para respostas de sim ou não).
